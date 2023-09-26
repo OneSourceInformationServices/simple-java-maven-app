03a04a1d70cef0d33593d06c4b5e7931ff9b8226
Imran-dnb <106813271+Imran-dnb@users.noreply.github.com>
2023-09-26 21:02:22 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello Worlds!";
+    private static final String MESSAGE = "Hello D&B!";
 
     public App() {}
 
